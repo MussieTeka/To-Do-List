f22d2e74550fb96ef70f3e9d046e139d873c8dd5
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-03-31 04:45:18 +0300

    Add array of simple to-do tasks to script.js

--- src/script.js
+++ src/script.js
@@ -8,16 +8,43 @@ const archiveBtn = document.getElementById('archive');
 const refreshIcon = document.querySelector('.fa-refresh');
 const downArrowIcon = document.querySelector('.fa-level-down');
 
+// Define initial to-do tasks
+const tasks = [
+  { description: 'Buy groceries', completed: false, index: 0 },
+  { description: 'Walk the dog', completed: false, index: 1 },
+  { description: 'Finish homework', completed: true, index: 2 },
+];
+
+// Render initial to-do tasks
+function renderTasks() {
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" ${task.completed ? 'checked' : ''} />
+      <span>${task.description}</span>
+      <i class="fa fa-ellipsis-v"></i>
+    `;
+    li.classList.toggle('completed', task.completed);
+    todoList.appendChild(li);
+  });
+}
+
 // Define functions
 function addItem(e) {
   e.preventDefault();
   if (input.value) {
     const li = document.createElement('li');
+    const newTask = {
+      description: input.value,
+      completed: false,
+      index: tasks.length,
+    };
     li.innerHTML = `
       <input type="checkbox" />
-      <span>${input.value}</span>
+      <span>${newTask.description}</span>
       <i class="fa fa-ellipsis-v"></i>
     `;
+    tasks.push(newTask);
     todoList.appendChild(li);
     input.value = '';
   }
@@ -26,11 +53,21 @@ function addItem(e) {
 function toggleItem(e) {
   if (e.target.tagName === 'INPUT') {
     const item = e.target.parentNode;
-    item.classList.toggle('completed');
+    const index = Array.from(todoList.children).indexOf(item);
+    tasks[index].completed = e.target.checked;
+    item.classList.toggle('completed', e.target.checked);
   } else if (e.target.classList.contains('fa-ellipsis-v')) {
     const item = e.target.parentNode;
     const prev = item.previousSibling;
     todoList.insertBefore(item, prev);
+    const currentIndex = Array.from(todoList.children).indexOf(item);
+    const prevIndex = currentIndex - 1;
+    [tasks[currentIndex], tasks[prevIndex]] = [
+      tasks[prevIndex],
+      tasks[currentIndex],
+    ];
+    tasks[currentIndex].index = currentIndex;
+    tasks[prevIndex].index = prevIndex;
   }
 }
 
@@ -40,6 +77,8 @@ function clearCompleted() {
   for (let i = items.length - 1; i >= 0; i--) {
     const item = items[i];
     if (item.classList.contains('completed')) {
+      const index = Array.from(todoList.children).indexOf(item);
+      tasks.splice(index, 1);
       todoList.removeChild(item);
     }
   }
@@ -54,4 +93,7 @@ form.addEventListener('submit', addItem);
 todoList.addEventListener('click', toggleItem);
 archiveBtn.addEventListener('click', clearCompleted);
 refreshIcon.addEventListener('click', refreshPage);
-downArrowIcon.addEventListener('click', addItem); // Add this line
+downArrowIcon.addEventListener('click', addItem);
+
+// Render initial tasks
+renderTasks();
